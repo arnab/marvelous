42a26a619178eaae060684e1b52b03c65e46e5f5
Arnab Deka <arnab.deka@gmail.com>
2014-05-26 08:17:49 +0530

    Read API credentials from env.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.9.1"]
-                 [org.clojure/data.json "0.2.4"]]
+                 [org.clojure/data.json "0.2.4"]
+                 [digest "1.4.4"]]
   :dev-dependencies [[print-foo "0.5.0"]])
--- src/marvelous/api.clj
+++ src/marvelous/api.clj
@@ -1,21 +1,38 @@
 (ns marvelous.api
   [:require
-   [clj-http.client :as http]
-   [clojure.data.json :as json]])
+   [clojure.string :as str :only [split]]
+   [clj-http.client :as http :only [get]]
+   [clojure.data.json :as json :only [read-str]]
+   [digest :as digest :only [md5]]])
 
-(def base-url "http://gateway.marvel.com:80/v1/public/")
+(def base-url "http://gateway.marvel.com/v1/public/")
+
+(defn- credentials
+  []
+  (str/split (System/getenv "MARVEL_CREDENTIALS") #"::"))
+
+(defn- epoch []
+  (int (/ (System/currentTimeMillis) 1000)))
+
+(defn- credentials-hash
+  [& args]
+  (digest/md5 (apply str args)))
 
 (defn- fetch-from-api
   "fetches data from Marvel's API"
   [resource limit offset]
-  (http/get
-   (str base-url resource) {
-    :query-params {
-     :limit limit
-     :offset offset
-     :apikey "552081f369860d155d50745c9688e1c8"
-     :ts "1"
-     :hash "a9b60cc028691a98ad8553782dec9580"}}))
+  (let [ts (epoch)
+        [api-key pvt-key] (credentials)]
+    (http/get
+     (str base-url resource)
+     {
+      :query-params
+      {
+       :limit limit
+       :offset offset
+       :apikey api-key
+       :ts ts
+       :hash (credentials-hash ts pvt-key api-key)}})))
 
 (defn- parse
   [s]
